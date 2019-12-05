d50ceb6dbfbe08a54d5a6c11bab5f6879ba39c9b
xuliwennew <624026015@qq.com>
2019-12-05 16:41:23 +0800

    day all

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,31 @@
-import moment from "moment"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    show: false,
+    actions: [
+      {
+        name: '选项'
+      },
+      {
+        name: '选项'
+      },
+      {
+        name: '选项',
+        subname: '副文本',
+        openType: 'share'
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-     console.log(moment().format("dddd"))
+  onClose() {
+    this.setData({ show: false });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onSelect(event) {
+    console.log(event.detail);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  show(){
+     this.setData({
+       show:!this.data.show
+     })
   }
-})
+});
